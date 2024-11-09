d89b7d1cb5087924a6050aa139d633c51d2b7648
deriifirgiawan <derifirgiawan025@gmail.com>
2024-11-09 20:06:40 +0700

    feat: add video

--- src/components/templates/Walikota/Banner/Banner.tsx
+++ src/components/templates/Walikota/Banner/Banner.tsx
@@ -1,22 +1,19 @@
-import { ImageConstant } from "@/constants";
-import Image from "next/image";
-
+"use client";
 export const Banner = () => {
   return (
-    <section className="pb-4 lg:pb-0 md:pb-0 mt-[72px] mx-[24px] lg:mx-[72px] md:mx-[72px] bg-[#171414] pt-12 flex items-center justify-between px-4 flex-col lg:flex-row md:flex-row ">
-      <Image
-        src={ImageConstant.BANNER_HARU}
-        width={1000}
-        height={400}
-        alt="Haru&Dhani"
-      />
-
-      <div className="text-center">
-        <h1 className="font-bold text-lg md:text-1xl lg:text-1xl italic">
-          "Bersama Haru Dhani, kita bangun Bandung yang tidak hanya maju, tapi
-          juga bersahaja dan berkeadilan. Saatnya Bandung lebih inklusif untuk
-          semua warganya."
-        </h1>
+    <section>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <video controls className="w-full h-[450px]">
+          <source src="/videos/1109.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </section>
   );
